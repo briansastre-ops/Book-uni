128c9f778ab4016ea308b314fb706c03c13ce28d
briansastre-ops <briansastre158@gmail.com>
2025-02-26 17:40:14 -0300

    Auth Form Sign-in Sign-up

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import { signUpSchema } from "@/lib/validations";
+import AuthForm from "@/components/AuthForm";
+const Page = () => (
+  <AuthForm
+    type="SIGN_UP"
+    schema={signUpSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+      fullName: "",
+      universityId: 0,
+      universityCard: "",
+    }}
+    onSubmit={() => {}}
+  />
+);
+export default Page;
